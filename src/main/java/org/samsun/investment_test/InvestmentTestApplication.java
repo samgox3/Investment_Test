6bf938575a1eb3b4cb6341dc03c38f4cd7b91630
samgox3 <samgox3@gmail.com>
2024-09-29 19:13:27 +0800

    Init

---
+++ src/main/java/org/samsun/investment_test/InvestmentTestApplication.java
@@ -0,0 +1,13 @@
+package org.samsun.investment_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentTestApplication.class, args);
+	}
+
+}
